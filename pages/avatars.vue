a7b795a3de46af58c17c412f6cbad7192d2faee1
MWeirich <matz.weirich@gmail.com>
2020-03-22 11:41:43 +0100

    make all cards in avatare same height

--- pages/avatars.vue
+++ pages/avatars.vue
@@ -1,19 +1,21 @@
 <template>
     <v-container>
-        <v-card-title class="display-2">Avatare</v-card-title>
-        <v-layout row justify-center align-center>
+        <h1 class="display-2 my-3">Avatare</h1>
+        <v-row justify-center align-start>
             <v-col v-for="avatar in avatars">
                 <v-hover v-slot:default="{ hover }">
                     <v-card class="pa-2" :elevation="hover ? 12 : 2" @click="select(avatar)">
-                        <v-card-title class="display-1">
-                            {{avatar.name}}
-                            <v-icon dark color="primary" v-if="avatar.id === user.avatar" size="40" class="ml-2">
-                                mdi-check
-                            </v-icon>
-                        </v-card-title>
-                        <AvatarImage :avatar="avatar.id"/>
-                        <v-card-text>{{avatar.introduction}}</v-card-text>
-                        <v-layout justify-center align-bottom>
+                        <div>
+                            <v-card-title class="display-1">
+                                {{avatar.name}}
+                                <v-icon dark color="primary" v-if="avatar.id === user.avatar" size="40" class="ml-2">
+                                    mdi-check
+                                </v-icon>
+                            </v-card-title>
+                            <AvatarImage :avatar="avatar.id"/>
+                            <v-card-text>{{avatar.introduction}}</v-card-text>
+                        </div>
+                        <v-layout justify-center align-end>
                             <v-chip v-for="tag in avatar.tags" text-color="primary" pill outlined color="primary">
                                 <b>{{tag}}</b>
                             </v-chip>
@@ -21,7 +23,7 @@
                     </v-card>
                 </v-hover>
             </v-col>
-        </v-layout>
+        </v-row>
     </v-container>
 </template>
 <script lang="ts">
@@ -57,6 +59,12 @@
 
     .v-card:not(.on-hover) {
         opacity: 1;
+    }
+
+    .col, .v-card {
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
     }
 
 </style>
