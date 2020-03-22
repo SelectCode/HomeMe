7d53085b8a67ab5a9559ae09829547bcbf9f41ac
Annabella Sophie Kadavanich <a.kadavanich@campus.tu-berlin.de>
2020-03-22 09:05:05 +0100

    Start with recommender

--- data/ITimeRecommender.ts
+++ data/ITimeRecommender.ts
@@ -1,9 +1,34 @@
+/* ------- Default Profile Settings ---- */
+const DEFAULT = 2;
+const WORKING_HOURS = 8;
+const MODEL = 'pomodoro';
+const
+
+
+
+/* ------------------------------------- */
+
 export interface ITimeRecommender {
     getRecommendedTimesForSettings(settings: Settings): Reminder[]
 }
 
 export class ITimeRecommenderImpl implements ITimeRecommender {
     getRecommendedTimesForSettings(settings: Settings): Reminder[] {
+        var timeArray: Reminder[] = [];
+
+        var amountBreaks = DEFAULT;
+
+        if(settings.breakModel === 'pomodoro')
+            amountBreaks = Math.floor((settings.workingHours*60) / 80);
+        else if(settings.breakModel === '90:20')
+            amountBreaks = Math.floor((settings.workingHours*60) / 80);
+        else if(settings.breakModel === '2x15')
+            amountBreaks = Math.floor((settings.workingHours*60) / 80);
+        else // individual
+            amountBreaks = Math.floor((settings.workingHours*60) / 80);
+
+
+
         return [];
     }
 }
@@ -14,7 +39,10 @@ export interface Settings {
     drinkingReminders: boolean,
     pauseReminders: boolean
     snackReminders: boolean,
-    lunchBreak: boolean
+    lunchBreak: boolean,
+    workingHours: number,
+    breakModel: string // input: pomodoro, 90:20, 2x15, individual
+    totalBreakDuration: number // in minutes
     //TODO: Add further settings
 }
 
