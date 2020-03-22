7e395ddcb05b98fd352b62a0755f4a03edff1eeb
Annabella Sophie Kadavanich <a.kadavanich@campus.tu-berlin.de>
2020-03-22 11:07:28 +0100

    Work on recommender

--- data/ITimeRecommender.ts
+++ data/ITimeRecommender.ts
@@ -1,49 +1,102 @@
-/* ------- Default Profile Settings ---- */
-const DEFAULT = 2;
-const WORKING_HOURS = 8;
-const MODEL = 'pomodoro';
-const
-
-
-
-/* ------------------------------------- */
-
 export interface ITimeRecommender {
+    /**
+     * This method is called when the user starts to work (also after a break)
+     * @param settings
+     */
     getRecommendedTimesForSettings(settings: Settings): Reminder[]
 }
 
 export class ITimeRecommenderImpl implements ITimeRecommender {
+
     getRecommendedTimesForSettings(settings: Settings): Reminder[] {
-        var timeArray: Reminder[] = [];
 
-        var amountBreaks = DEFAULT;
+        let pauses: Reminder[] = [];
+        let amountBreaks : number = 2;
+        let breakDuration = settings.breakDuration;
 
-        if(settings.breakModel === 'pomodoro')
-            amountBreaks = Math.floor((settings.workingHours*60) / 80);
-        else if(settings.breakModel === '90:20')
-            amountBreaks = Math.floor((settings.workingHours*60) / 80);
-        else if(settings.breakModel === '2x15')
-            amountBreaks = Math.floor((settings.workingHours*60) / 80);
-        else // individual
-            amountBreaks = Math.floor((settings.workingHours*60) / 80);
+        if(settings.lunchBreak){
+            let lunchDuration : number = settings.lunchBreakTime;
+            breakDuration -= lunchDuration;
 
+        }
+        // Has children?
+        if(!settings.childrenAtHome){
+            amountBreaks = Math.floor((breakDuration/5) + 1);
+            let timeBetweenBreaks : number = Math.floor((settings.workingHours*60) / amountBreaks);
+            pauses.push(...this.generatePauses(amountBreaks, timeBetweenBreaks));
+        }
 
+        // Generate drinking pauses
+        if (settings.drinkingReminders) {
 
-        return [];
+            pauses.push(...this.generateDrinkingPauses(settings.workingHours))
+        }
+
+        if(settings.snackReminders){
+            pauses.push(...this.generateSnackPauses(settings.workingHours))
+        }
+
+        return pauses;
     }
+
+    /**
+     * Generate a pause every 30 minutes
+     */
+    generatePauses(amountBreaks : number, timeBetweenBreaks: number): Reminder[] {
+        let pauses: Reminder[] = [];
+        for (let i = 1; i < amountBreaks; i++) {
+            pauses.push({
+                type: "break",
+                inMinutes: i * timeBetweenBreaks + (i-1) * 5
+            })
+        }
+        return pauses;
+    }
+
+
+    /**
+     * Generate a drinking pause every 30 minutes
+     */
+    generateDrinkingPauses(workingHours : number): Reminder[] {
+        let pauses: Reminder[] = [];
+        let iterations = (workingHours*60)/30;
+        for (let i = 0; i < iterations; i++) {
+            pauses.push({
+                type: "drinking",
+                inMinutes: (i + 1) * 30
+            })
+        }
+        return pauses;
+    }
+
+    /**
+     * Generate a snack pause every 30 minutes
+     */
+    generateSnackPauses(workingHours : number): Reminder[] {
+        let iterations = (workingHours*60)/30;
+        let pauses: Reminder[] = [];
+        for (let i = 0; i < iterations; i++) {
+            pauses.push({
+                type: "snack",
+                inMinutes: (i + 1) * 30
+            })
+        }
+        return pauses;
+    }
+
 }
 
 export interface Settings {
     avatarId: string,
     childrenAtHome: boolean,
     drinkingReminders: boolean,
-    pauseReminders: boolean
+    //pauseReminders: boolean => Assumption: A user without kids wants take 5min pauses
     snackReminders: boolean,
     lunchBreak: boolean,
+    lunchBreakTime: number, // evtl. in Date ändern
+    breakDuration: number,
     workingHours: number,
-    breakModel: string // input: pomodoro, 90:20, 2x15, individual
-    totalBreakDuration: number // in minutes
-    //TODO: Add further settings
+    mood: string
 }
 
 export interface Reminder {
