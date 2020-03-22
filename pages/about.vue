da71f817711eab8605629b8dfaf721254ab82a07
Florian Baader <florian.baader@selectcode.de>
2020-03-22 19:43:03 +0100

    add link

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,11 @@
 <template>
-    <v-layout align-center justify-center>
+    <v-layout align-center justify-center column>
         <iframe width="840" height="472" src="https://www.youtube.com/embed/VRAiHnKdzVk" frameborder="0"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen></iframe>
+        <v-btn color="primary" target="_blank" href="https://devpost.com/software/personalisierter-home-office-begleiter" class="ma-3">
+            DevPost Project
+            <v-icon>mdi-link</v-icon>
+        </v-btn>
     </v-layout>
 </template>
