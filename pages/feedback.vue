c679698b3d22328d06f876e96785377897d8ae7c
Florian Baader <florian.baader@selectcode.de>
2020-03-23 15:09:01 +0100

    next try

--- pages/feedback.vue
+++ pages/feedback.vue
@@ -2,10 +2,12 @@
     <v-layout align-center justify-center column>
         <h2 class="display-2 text-center ma-3">Feedback & Contact 🦜</h2>
         <v-card id="contact" class="pa-2">
-            <v-form name="contact" data-netlify-recaptcha="true" method="POST" data-netlify="true"
-                    action="/thanks"
-                    data-netlify-honeypot="bot-field" class="pa-2">
-                <input type="hidden" name="form-name" value="contact" />
+            <v-form name="contact"
+                    data-netlify-recaptcha="true"
+                    method="POST"
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                    class="pa-2">
                 <v-text-field id="name" label="Name" rounded full-width filled/>
                 <v-text-field id="email" label="Email" rounded full-width filled type="email"/>
                 <v-textarea id="message" label="Message" rounded full-width filled auto-grow/>
@@ -18,7 +20,7 @@
     </v-layout>
 </template>
 <style>
-    #contact{
+    #contact {
         width: 50vw;
     }
 </style>
