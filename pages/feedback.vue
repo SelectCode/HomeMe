22af5074bc953c21810ae2020846fd58e4a8e4a8
Florian Baader <florian.baader@selectcode.de>
2020-03-23 17:03:08 +0100

    try manual submit

--- pages/feedback.vue
+++ pages/feedback.vue
@@ -8,17 +8,51 @@
                     data-netlify="true"
                     data-netlify-honeypot="bot-field"
                     class="pa-2">
+                <input type="hidden" name="form-name" value="contact"/>
                 <v-text-field id="name" label="Name" rounded full-width filled/>
                 <v-text-field id="email" label="Email" rounded full-width filled type="email"/>
                 <v-textarea id="message" label="Message" rounded full-width filled auto-grow/>
                 <div data-netlify-recaptcha="true"></div>
-                <v-btn type="submit" color="primary" rounded large block>
+                <v-btn @click="submit" color="primary" rounded large block>
                     Submit
                 </v-btn>
             </v-form>
         </v-card>
     </v-layout>
 </template>
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class FeedbackComponent extends Vue {
+
+        form = {
+            name: '',
+            message: '',
+            email: ''
+        }
+
+
+        encode(data: any) {
+            return Object.keys(data)
+                .map(
+                    key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+                )
+                .join("&");
+        }
+
+        async submit() {
+            await this.$axios.post('https://pause.selectcode.de/feedback',
+                this.encode({
+                    "form-name": "ask-question",
+                    ...this.form
+                }),
+                {
+                    headers: {"Content-Type": "application/x-www-form-urlencoded"}
+                })
+        }
+    }
+</script>
 <style>
     #contact {
         width: 50vw;
