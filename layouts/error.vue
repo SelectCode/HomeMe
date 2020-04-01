2364263cfbe794f27b612052a2906014c8e2d960
MWeirich <matz.weirich@gmail.com>
2020-04-01 21:01:38 +0200

    better error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,44 +1,48 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+    <v-container fluid fill-height>
+        <v-row align="center" justify="center">
+            <div class="text-center">
+                <h1 class="display-1" v-if="error.statusCode === 404">
+                    {{ pageNotFound }}
+                </h1>
+                <h1 class="display-1" v-else>
+                    {{ otherError }}
+                </h1>
+                <NuxtLink class="display-1" to="/">
+                    Home page
+                </NuxtLink>
+            </div>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-export default {
-  layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null
+    export default {
+        layout: 'empty',
+        props: {
+            error: {
+                type: Object,
+                default: null
+            }
+        },
+        data() {
+            return {
+                pageNotFound: '404 Not Found',
+                otherError: 'An error occurred'
+            }
+        },
+        head() {
+            const title =
+                this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+            return {
+                title
+            }
+        }
     }
-  },
-  data () {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
-  },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
-  }
-}
 </script>
 
 <style scoped>
-h1 {
-  font-size: 20px;
-}
+    h1 {
+        font-size: 20px;
+    }
 </style>
