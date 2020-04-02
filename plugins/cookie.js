dcaa122aa64f2f97f4284881cbf07efaa69f3a2d
Restyled.io <commits@restyled.io>
2020-04-02 07:58:47 +0000

    Restyled by prettier

--- plugins/cookie.js
+++ plugins/cookie.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
-import CookieLaw from 'vue-cookie-law'
+import Vue from "vue";
+import CookieLaw from "vue-cookie-law";
 
-Vue.component('cookie-law', CookieLaw)
+Vue.component("cookie-law", CookieLaw);
